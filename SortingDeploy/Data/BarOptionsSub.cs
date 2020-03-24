691b000b6a021f0ad061b8bdff983212a9c4056e
zotyovegh <zotyovegh@gmail.com>
2020-03-24 18:51:56 +0100

    Animation issue solved

---
+++ SortingDeploy/Data/BarOptionsSub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChartJs.Blazor.ChartJS.BarChart;
+using ChartJs.Blazor.ChartJS.Common.Properties;
+
+namespace SortingDeploy.Data
+{
+    public class BarOptionsSub : BarOptions
+    {
+        /// <summary>
+        /// General animation time
+        /// </summary>
+        public Animation Animation { get; set; }
+    }
+}
