ebcaf3f2d6906168c95598f5d7c48ec3cb11a811
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-03-26 01:41:28 +0100

    Basic Cleanup

--- SortingDeploy/Data/SortingMethods.cs
+++ SortingDeploy/Data/SortingMethods.cs
@@ -9,9 +9,7 @@ using Index = SortingDeploy.Pages.Index;
 namespace SortingDeploy.Data
 {
     public class SortingMethods
-
-    {
-        
+    {        
         public void CreateRandomArray(int arraySize, int[] array, int max)
         {
             Random random = new Random();
@@ -20,214 +18,74 @@ namespace SortingDeploy.Data
                 array[i] = random.Next(1, max);
             }
         }
-
         public async Task BubbleSort(int[] array, int arraySize, Index index, int time)
         {
-            int delay = TimeCalculation(time, "Bubble Sort");
             for (int i = arraySize - 1; i > 0; i--)
             {
                 if (i == arraySize - 1)
                 {
-                    await index.Update(array);
-                    //Thread.Sleep(1000);
+                    await index.Update(array);                    
                 }
                 for (int j = 0; j < i; j++)
                 {
                     if (array[j] > array[j + 1])
                     {
-                        Thread.Sleep(delay);
-                        SwapArrayValues(array, j, j + 1); 
-                        
-                        await index.Update(array);
-                        
-                        Debug.WriteLine("We are here");
+                        Thread.Sleep(time);
+                        SwapArrayValues(array, j, j + 1);                         
+                        await index.Update(array);                        
                     }
                 }
             }
-            Debug.WriteLine("Most kellene");
-            Thread.Sleep(1000);
-            //index.Enable();
-            Debug.WriteLine("Most kellett volna");
-            //await index.DisableCheck();
+            index.DisableCheck();            
         }
-
         public void SwapArrayValues(int[] array, int firstPos, int secondPos)
         {
             int temp = array[firstPos];
             array[firstPos] = array[secondPos];
             array[secondPos] = temp;
         }
-
         public async Task SelectionSort(int[] array, int arraySize, Index index, int time)
         {
-            int delay = TimeCalculation(time, "Selection Sort");
             for (int x = 0; x < arraySize - 1; x++)
             {
-                //if (x == 0)
-                //{
-                //    await index.Update(array);
-                //    //Thread.Sleep(1000);
-                //}
                 Debug.WriteLine("X: " + x);
-                int min = x;
-                
+                int min = x;                
                 for (int y = x + 1; y < arraySize; y++)
                 {
                     Debug.WriteLine("Y: " + y);
                     if (array[y] < array[min])
                     {
                         min = y;
-                    }
-                    
+                    }                    
                 }
                 
                 SwapArrayValues(array, x, min);
                 await index.Update(array);
-                Thread.Sleep(delay);
+                Thread.Sleep(time);
             }
+            index.DisableCheck();
         }
-
         public async Task InsertionSort(int[] array, int arraySize, Index index, int time)
         {
-            int delay = TimeCalculation(time, "Insertion Sort");
             for (int i = 1; i < arraySize; i++)
             {
                 if (i == 1)
                 {
                     await index.Update(array);
-                    //Thread.Sleep(1000);
                 }
                 int temp = i;
                 int numToInsert = array[i];
                 while ((temp > 0) && (array[temp - 1] > numToInsert))
                 {
-
                     array[temp] = array[temp - 1];
                     temp--;
                     await index.Update(array);
-                    Thread.Sleep(delay);
+                    Thread.Sleep(time);
                 }
-
-                array[temp] = numToInsert;
-                
+                array[temp] = numToInsert;                
             }
+            index.DisableCheck();
         }
 
-        private string stro;
-        
-        public async Task QuickSortCall(int[] array, int start, int end, Index index, int time)
-        {
-            int delay = TimeCalculation(time, "Quick Sort");
-            await QuickSort(array, start, end, index, delay );
-            await index.Update(array);
-            //Debug.WriteLine("Befejezte");
-        }
-       
-        //public async void QuickSort(int[] array, int left, int right, Index index)
-        //{
-        //    int i = left;
-        //    int j = right;
-
-        //    var pivot = array[(left + right) / 2];
-
-        //    while (i <= j)
-        //    {
-
-        //        while (array[i] < pivot)
-
-        //            i++;
-
-        //        while (array[j] > pivot)
-        //            j--;
-
-        //        if (i <= j)
-        //        {
-        //            var tmp = array[i];
-        //            array[i] = array[j];
-        //            array[j] = tmp;
-        //            Thread.Sleep(10);
-        //            i++;
-        //            j--;
-        //        }
-        //    }
-
-        //    if (left < j)
-        //        QuickSort(array, left, j, index);
-        //    await index.Update(array);
-        //    Thread.Sleep(timeQuick);
-        //    if (i < right)
-        //        QuickSort(array, i, right, index);
-        //    await index.Update(array);
-        //    Thread.Sleep(timeQuick);
-        //}
-
-        public async Task QuickSort(int[] array, int start, int end, Index index, int time)
-        {
-            int i;
-            if (start < end)
-            {
-                
-                i = Partition(array, start, end, time, index);
-                
-                await QuickSort(array, start, i - 1, index, time);
-                
-                await QuickSort(array, i + 1, end, index, time);
-                Debug.WriteLine("hapci");
-            }
-            
-        }
-
-        private int Partition(int[] array, int start, int end, int delay, Index index)
-        {
-
-            int temp;
-            int p = array[end];
-            int i = start - 1;
-
-            for (int j = start; j <= end - 1; j++)
-            {
-                //Thread.Sleep(delay);
-                if (array[j] <= p)
-                {
-                    i++;
-                    //temp = array[i];
-                    //array[i] = array[j];
-                    //array[j] = temp;
-                    SwapArrayValues(array, i, j);
-                    index.Update(array);
-                    Thread.Sleep(delay);
-                }
-            }
-
-            temp = array[i + 1];
-
-            array[i + 1] = array[end];
-            array[end] = temp;
-            return i + 1;
-        }
-
-        private int TimeCalculation(int time, string sort)
-        {
-            int delay = 50;
-            if (sort == "Bubble Sort")
-            {
-                delay = time * 7;
-            }
-            else if (sort == "Selection Sort")
-            {
-                delay = time * 7;
-            }
-            else if (sort == "Insertion Sort")
-            {
-                delay = time * 7;
-            }
-            else if (sort == "Quick Sort")
-            {
-                delay = time * 4;
-            }
-
-            return delay;
-
-        }
     }
 }
